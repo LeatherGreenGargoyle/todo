00c789573588b698b873d57c3c0c09e9532d333b
leathergreengargoyle <avtran23@gmail.com>
2017-07-11 09:34:50 -0700

    [progress]Server routes

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.use(morgan('dev'))
 
-// require('./routes')(app)
+require('./routes')(app)
 
 const port = process.env.PORT || 3000
 app.listen(port, () => {
