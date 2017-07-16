480fd4d2ebe1627e0bfbf94c29b256d0c2c924ca
leathergreengargoyle <avtran23@gmail.com>
2017-07-16 10:53:54 -0700

    [progress]Create lists styles

---
+++ src/components/lists/style.js
@@ -0,0 +1,11 @@
+import StyleSheet from 'react-native'
+
+export const textInputStyles = StyleSheet.create({
+  height: 40,
+  borderColor: 'gray',
+  borderWidth: 1,
+})
+
+export const listStyles = StyleSheet.create({
+  flex: 1,
+})
